0ef6b854b17f91255695e510a5e3503b95ca4702
Maciek Lewandowski <ext-maciej.lewandowski@lindorff.com>
2018-03-05 12:39:44 +0100

    Refactor to modules and getting single product details fix

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { IProduct } from './product';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
+
 
 @Injectable()
 export class ProductService {
@@ -13,9 +14,13 @@ export class ProductService {
 
     constructor(private _httpClient: HttpClient) { }
 
+    getProduct(id: number):   Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
+    }
+
     getProducts(): Observable<IProduct[]> {
         return this._httpClient.get<IProduct[]>(this._productUrl)
-        .do(data => console.log('All: ' + JSON.stringify(data)))
         .catch(this.handleError);
     }
 
